7c4a3e762eedb201cac7178474b972260f8c309a
livelaughriley <34988536+livelaughriley@users.noreply.github.com>
2019-07-15 14:31:38 -0500

    work with wrapr.R (not really a lambda-esque but I wanted to try it out)

---
+++ R/wrapr.R
@@ -0,0 +1,77 @@
+#
+# wrapr.R
+# 2019-07-15 12:24:06
+#
+#
+# Practice with the `wrapr` package
+# https://github.com/winvector/wrapr
+# 
+
+library(wrapr)
+
+# assign global vars ------------------------------------------------------
+
+# assign vectors
+vec_a <- 1:100
+vec_b <- 4:103
+
+
+# simple math -------------------------------------------------------------
+
+
+# using lambda() function
+add_func <- lambda(a, b, a + b)
+add_func(vec_a, vec_b)
+
+# using brace interface
+add_func <- a~b := { a + b }
+add_func(vec_a, vec_b)
+
+
+
+# substitution with `let` and `%.>%` --------------------------------------
+
+# base R
+base_func <- function(a, b) {
+    x <- a
+    y <- b
+    x %*% y
+}
+base_func(vec_a, vec_b)
+
+# wrapr
+let_func <- function(a, b) {
+    let(c(x = quote(a), y = quote(b)), 
+        expr = {
+            x %*% y
+        }
+    )
+}
+let_func(vec_a, vec_b)
+
+
+# comparing speed of base R and wrapr functions
+
+set.seed(1)
+
+mx_a <- matrix(
+    runif(1000 * 1000),
+    nrow = 1000,
+    ncol = 1000
+)
+
+mx_b <- matrix(
+    runif(1000 * 100),
+    nrow = 1000,
+    ncol = 100
+)
+
+results <- microbenchmark(
+    "base_func" = {
+        base_func(mx_a, mx_b)
+    },
+    "let_func" = {
+        let_func(mx_a, mx_b)
+    },
+    times = 100
+)
